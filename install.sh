203c6237bc3c1148c67e9458fd79f6dc851d2e6f
coltstrgj <coltstrgj@gmail.com>
2014-09-24 14:03:22 -0600

    Added instructions: contains preface for "userbashrc" install.sh: now creates the file userbashrc in the modules section     moves the existing .bashrc to ~/.config/terrorshell/modules/userbashrc so that all pre existing     aliases will persist templates/bashrc: added userbashrc to the list of modules TODO Learn how commit messages work TODO check existing bashrc for conflicting alias names

--- install.sh
+++ install.sh
@@ -8,12 +8,17 @@ command -v git &> /dev/null && git clone https://github.com/terrorbyte/terrorshe
 	echo "Git not found"
 	exit
 }
-echo "Backing up old bashrc to ~/.bashrc.orig..."
+echo -e "Backing up old bashrc to ~/.config/terrorshell/modules/bashrc.orig...\nSee this file for more information on modules"
 if [ -f ~/.bashrc ] || [ -h ~/.bashrc ]; then
-  mv ~/.bashrc ~/.bashrc.orig;
+    touch ~/.config/terrorshell/modules/userbashrc
+    cat ~/.config/terrorshell/instructions > ~.config/terrorshell/modules/bashrc.orig
+    cat ~.bashrc >> ~/.config/terrorshell/modules/bashrc.orig
+    rm ~/.bashrc 
 fi
 echo "Replacing .bashrc..."
 cp ~/.config/terrorshell/templates/bashrc ~/.bashrc
+echo "You should never need to use the install script again"
+echo -e "Simply use the command 'terrorshell update'\nSee 'terrorshell help' for other options\n\n"
 echo "Welcome to a real shell:"
 echo "
 ___________                               _________.__           .__  .__
